00470918f7cddc65439f6f66fa8dd27cd94b28ab
Mohamed Essakili <captainmed510@gmail.com>
2023-07-16 19:32:06 +0100

    Authentication/Profile

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,8 +15,14 @@ use App\Http\Controllers\Api\V1\Auth;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('profile', [Auth\ProfileController::class, 'show']);
+    Route::put('profile', [Auth\ProfileController::class, 'update']);
+    Route::put('password', Auth\PasswordUpdateController::class);
+    Route::post('auth/logout', Auth\LogoutController::class);
 });
-
 Route::post('auth/register', Auth\RegisterController::class);
+Route::post('auth/login', Auth\LoginController::class);
+
+
+
